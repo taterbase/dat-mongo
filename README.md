199868ad13d065c641bb53ca8ab189023f767a0d
taterbase <shankga@gmail.com>
2014-10-21 18:33:20 -0600

    initial

---
+++ README.md
@@ -0,0 +1,30 @@
+#dat-mongo
+
+A [MongoDB](http://mongodb.org) plugin for [dat](http://dat-data.com) for realtime data changes
+
+##Usage
+
+Create a `dat.json` with the `hooks.listen` plugin identified and a `mongoOplogUrl` defined.
+
+```json
+{
+  "name": "dat-mongo",
+
+  "hooks": {
+    "listen": "dat-mongo"
+  },
+
+  "mongoOplogUrl": "127.0.0.1:27017/local"
+}
+```
+
+Now `dat listen`.
+
+##Caveats
+
+This requires your Mongo instance to be running as a [replica set](http://docs.mongodb.org/manual/reference/replica-configuration/) so that operations are written to the [oplog](http://docs.mongodb.org/manual/core/replica-set-oplog/).
+
+
+___
+
+Made with ⚡️ by [@taterbase](https://twitter.com/taterbase)
---
+++ index.js
@@ -0,0 +1,56 @@
+var tail = require('oplog-transform-tail')
+
+module.exports = function hook(dat, done) {
+  console.log("OH HI")
+
+  tail({
+
+    url: dat.options.mongoOplogUrl,
+
+    insert: function(_id, doc, cb) {
+      console.log("AYYYYY")
+      dat.put(_id, doc, function(err) {
+        if (err)
+          console.log(err)
+
+        cb(err)
+      })
+    },
+
+    update: function(_id, ready) {
+      dat.get(_id, function(err, retrievedDoc) {
+        if (err)
+          return console.log(err)
+
+        if (!retrievedDoc)
+          return console.log("Doc with _id " + _id + " not in dat, could not update")
+
+        var version = retrievedDoc.version
+
+        ready(retrievedDoc, function(err, updatedDoc, cb) {
+          updatedDoc.version = version
+          dat.put(_id, updatedDoc, function(err) {
+            if (err)
+              console.log(err)
+
+            cb(err)
+          })
+        })
+          
+      })
+    },
+
+    remove: function(_id, cb) {
+      dat.delete(_id, function(err) {
+        if (err)
+          console.log(err)
+
+        cb(err)
+      })
+    }
+
+  })
+
+  done()
+
+}
